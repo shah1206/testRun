06592cbef9b4ef5c5a09354bd616e32ae0b11c7d
shah1206 <pari.shah1206@gmail.com>
2021-01-23 11:38:01 -0500

    changed the name of the ground

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup(){
     world = engine.world;
 
 
-    ground = new Ground(600,height,1200,20);
+    ground1 = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -46,7 +46,7 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    ground.display();
+    ground1.display();
     pig1.display();
     log1.display();
 
